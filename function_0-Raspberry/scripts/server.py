0e9975c0657d327e7c475df855406e4434948701
Neluji <38362829+Neluji@users.noreply.github.com>
2022-01-19 22:53:15 +0100

    Export modifications made offline on Jetson & RPi

--- function_0-Raspberry/scripts/server.py
+++ function_0-Raspberry/scripts/server.py
@@ -1,3 +1,4 @@
+
 # coding: utf-8
 from threading import Thread
 import time
@@ -7,6 +8,7 @@ import struct
 import socket
 from math import tan,pi
 from simple_pid import PID
+import csv
 
 HOST = ''     # IP address on LAN
 PORT = 6666              # Arbitrary non-privileged port
@@ -27,15 +29,20 @@ CAR_LENGTH = 60
 CAR_WIDTH = 40
 CAR_LEN_MM = 600
 
-TARGET_DIST_MM = 1500
+TARGET_DIST_MM = 1700
 
 MAX_ERROR_MM = 2000
 MIN_ERROR_MM = -2000
 
-CORRECT_GAIN = ((MAX_SPEED_FW - MAX_SPEED_BW)/(MAX_ERROR_MM - MIN_ERROR_MM))
-PID_P = 1.5
-PID_I = 0.5
+PID_P = 0.02
+PID_I = 0.0002
 PID_D = 0
+
+#listpos=[]
+#listspeed=[]
+#listangle=[]
+#time0=time.time()
+#listtime = []
 
 class EthReceiver(Thread):
     def __init__(self,connSock, canBus):
@@ -45,13 +52,16 @@ class EthReceiver(Thread):
         self.distance = 0
         self.angle = 0
         self.speed_cmd = 0
+        self.enable_speed = 0
         self.speed_left = 0
         self.speed_right = 0
         self.steering = 0
         # PID object : P,I, and D gain, and setpoint
-        self.pid = PID(CORRECT_GAIN * PID_P, PID_I, PID_D)
+        self.pid = PID(PID_P, PID_I, PID_D)
+        self.pid.output_limits = (-25,25)
+        self.prev_sampling = time.time()
 
-    def run(self):
+    def run(self): 
         self.distance = 0
         self.angle = 0
         self.enable_speed = 0
@@ -69,20 +79,12 @@ class EthReceiver(Thread):
             # Receive data from Jetson (eth)
             fromJetson = self.connSock.recv(1024)
             
-            # Leave if no more data coming from Jetson (= eth link lost)
-            '''
-            if not fromJetson: 
-                print('No data from Jetson')
-                break
-            '''
 
-            # Split data between distance (1st 8 B) & angle (following 8 B)
-            #rawDist = fromJetson[0:8]
-            #rawAngle = fromJetson[8:16]
             fromJetson = fromJetson.decode('utf-8').split(":")
             try:
                 rawDist = fromJetson[0]
                 rawAngle = fromJetson[1]
+                self.enable_speed = int(fromJetson[2])
                 if rawDist: self.distance = int(rawDist)
                 if rawAngle: self.angle = int(rawAngle)
             except:
@@ -90,44 +92,46 @@ class EthReceiver(Thread):
                 break
 
 
-            #print("d : ", self.distance, " ; a : ", self.angle)
-
+            
             # Update speed cmd according to the distance ()
             self.distance -= CAR_LEN_MM
-            self.enable_speed = True
             erreur = TARGET_DIST_MM - self.distance
-            # if (erreur < -MAX_ERROR_MM):
-            #     self.speed_cmd = MAX_SPEED_BM
-            # elif (erreur > MAX_ERROR_MM):
-            #     self.speed_cmd = MAX_SPEED_FW
-            # else:
-            #     # self.speed_cmd = int(K_GAIN*CORRECT_GAIN*erreur) + 50
-            self.speed_cmd = int(self.pid(erreur)) + 50
+
+            if not self.enable_speed: # Reset PID
+                self.pid.set_auto_mode(False,last_output=0)
+                self.pid.set_auto_mode(True,last_output=0)
+                erreur = 0
+
+            # Compute speed command w/ PID
+            
+            self.speed_cmd = int(self.pid(erreur)) +50
             if (self.speed_cmd > MAX_SPEED_FW): self.speed_cmd = MAX_SPEED_FW
             if (self.speed_cmd < MAX_SPEED_BW): self.speed_cmd = MAX_SPEED_BW
 
-            print("d>",self.distance,"/ a>",self.angle,"/ s>",self.speed_cmd)
+            print("d>",self.distance,"/ a>",self.angle,"/ s>",self.speed_cmd,"/ e>",self.enable_speed)
 
-            # Enable or not speed in motor command
-            if (self.enable_speed):
-                self.speed_cmd |= (1 << 7)
-            else:
-                self.speed_cmd &= ~(1 << 7)
+            #get data to write them into a csv
+            #listpos.append(self.distance)
+            #listangle.append(self.angle)
+            #listspeed.append(self.speed_cmd)
+            #listtime.append(time.time()-time0)
 
-            # Compute differential speed for motors
+            # Enable speed bit in motor colistdist.append(self.distance)
+            self.speed_cmd |= (1 << 7)
+
+            # Compute differential speed for motors NOT USED IN THE FINAL VERSION
             # self.speed_left = int(((CAR_LENGTH + CAR_WIDTH * tan(2*pi*(self.angle/360)))/CAR_LENGTH) * self.speed_cmd) + 50
             # self.speed_right = int(((CAR_LENGTH - CAR_WIDTH * tan(2*pi*(self.angle/360)))/CAR_LENGTH) * self.speed_cmd) + 50
 
             # Compute the steering from the angle
-            if self.angle < -25:
+            if self.angle < -25: #If we are at less than 25° from the robot, we steer at the maximum in this direction
                 self.steering = 0
-            elif self.angle > 25:
+            elif self.angle > 25: #same thing in the other direction
                 self.steering = 100
             else:
-                self.steering = 2*(self.angle+25) | (1 << 7)
+                self.steering = 2*(self.angle+25) | (1 << 7)  #Else we interpolate beetween these two points to get the corresponding steering
 
             # Compose & send CAN message to Nucleo
-            # toNucleo = can.Message(arbitration_id=SSC,data=[self.speed_left, self.speed_right,0,self.steering],extended_id=False)
             toNucleo = can.Message(arbitration_id=MCM,data=[self.speed_cmd, self.speed_cmd,0,self.steering],extended_id=False)
             self.canBus.send(toNucleo)
 
@@ -137,6 +141,11 @@ class EthReceiver(Thread):
         self.canBus.send(stopNucleo)
 
         self.connSock.close()
+
+        #with open('liste.csv','w',newline='') as f: #Write the data into a csv file at the end of the program
+            #writer=csv.writer(f)
+            #for i in range(len(listpos)):
+                #writer.writerow([listpos[i],listangle[i],listspeed[i],listtime[i]])
 
 if __name__ == "__main__":
 
--- function_2-Jetson/JetsonFollow.cpp
+++ function_2-Jetson/JetsonFollow.cpp
@@ -12,6 +12,8 @@
 #include <opencv2/highgui/highgui_c.h>
 #include <opencv2/core/ocl.hpp>
 
+#include <chrono>
+
 //include LIDAR
 #include <rplidar.h>
 
@@ -22,6 +24,18 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <string.h>
+#include <string>
+
+//include thread
+#include <istream>
+#include <sstream>
+#include <fstream>
+#include <streambuf>
+#include <thread>
+#include <fcntl.h>
+#include <netdb.h>
+#include <sys/types.h>
+
 
 #define PORT 6666
 #ifndef _countof
@@ -32,7 +46,123 @@ using namespace cv;
 using namespace rp::standalone::rplidar;
 using namespace std;
 
-//-------------------IMAGE RECOGNITION
+bool veryLost = false;
+int clientSocket;
+int listening;
+
+/*
+When the robot is lost, the function createServLost() is launched.
+This function create a server with a socket and  wait for client to connect.
+Once one client is connected the listening socket send the content of
+the website.
+*/
+void createServLost(){
+    cout << "Thread server launched" << endl;
+
+    std::string imageBase64;
+    std::string line;
+
+    // Encode the last image taken by the camera in base64
+    system("base64 imageLost.jpg > image.txt");
+
+    // Open and read image.txt to get the image in base64
+    std::ifstream image;
+    image.open("image.txt", std::ifstream::in);;
+    if (image.good()) {
+        imageBase64 += "<img src=\"data:image/gif;base64,";
+        while (getline(image,line)) {
+            imageBase64 += line;
+        }
+        imageBase64 += "\" class=\"responsive\" >";
+        image.close();
+    }
+    else cout<< "no Image found"<<endl;
+
+    // Open and read index.html and add the image
+    std::string content;
+    std::ifstream html;
+    int i = 0;
+    html.open("index.html", std::ifstream::in);;
+    if (html.good()) {
+        while (getline(html,line)) {
+            i++;
+            if (i == 82)
+                content += imageBase64;
+            else
+                content += line;
+        }
+        html.close();
+    }
+    else cout<<"html not found" <<endl;
+
+    // Create a socket
+    listening = socket(PF_INET, SOCK_STREAM, 0);
+    fcntl(listening, F_SETFL, O_NONBLOCK);
+    if (listening == -1)
+    {
+        cerr << "Can't create a socket! Quitting" << endl;
+        return;
+    }
+    else {
+	cout << "socket ok" << endl;
+    }
+ 
+    // Bind the ip address and port to a socket
+    sockaddr_in hint;
+    hint.sin_family = AF_INET;
+    hint.sin_port = htons(8080);
+    inet_pton(AF_INET, "127.0.0.1", &hint.sin_addr);
+ 
+    bind(listening, (sockaddr*)&hint, sizeof(hint));
+    listen(listening, SOMAXCONN);
+    // Wait for a connection
+    sockaddr_in client;
+    socklen_t clientSize = sizeof(client);
+
+    // The listening socket loop until a connection or the robot is not lost anymore
+    while(veryLost) {
+        //cout<<"loop"<<endl;
+        clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
+        if (clientSocket != -1) {
+	    //cout << "something" << endl;
+	    break;
+	}
+    }
+ 
+    if (clientSocket != -1) {
+        char host[NI_MAXHOST];
+        char service[NI_MAXSERV];
+
+        memset(host, 0, NI_MAXHOST);
+        memset(service, 0, NI_MAXSERV);
+    
+        if (getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0) {
+            cout << host << " connected on port " << service << endl;
+        }
+        else {
+            inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
+            cout << host << " connected on port " << ntohs(client.sin_port) << endl;
+        }
+
+        // Write the html back to the client
+        std::ostringstream oss;
+        oss << "HTTP/1.1 " << 200 << " OK\r\n";
+        oss << "Cache-Control: no-cache, private\r\n";
+        oss << "Content-Type: text/html\r\n";
+        oss << "\r\n";
+        oss << content;
+
+        std::string output = oss.str();
+        int size = output.size() + 1;
+
+        send(clientSocket, output.c_str(), size, 0);
+    }
+    else {
+        cout << "no connection" << endl;  
+    }
+    close(listening);
+
+}
 void DrawCircle(int posx, int posy, Mat & m, bool isMain) { //Draw circles, used for debugging 
 	if (isMain){ circle(m, Point(posx, posy), 60, 150, 20); }
 	else{ circle(m, Point(posx, posy), 20, 150, 5); }
@@ -52,21 +182,109 @@ int xToTheta(int x, int width, int FOV) {
 	int theta = (FOV/2)*(x-width/2)/(width/2);
 	return theta;
 }
+static double angle(cv::Point pt1, cv::Point pt2, cv::Point pt0)
+{
+	double dx1 = pt1.x - pt0.x;
+	double dy1 = pt1.y - pt0.y;
+	double dx2 = pt2.x - pt0.x;
+	double dy2 = pt2.y - pt0.y;
+	return (dx1 * dx2 + dy1 * dy2) / sqrt((dx1 * dx1 + dy1 * dy1) * (dx2 * dx2 + dy2 * dy2) + 1e-10);
+}
+int RedTriangle(Mat & m, int& sqX, int& sqY, std::chrono::milliseconds stopDuration)
+{
+	int tolerance = m.rows/10;
+	Mat bgIsolation; //detected orange-red part of the picture
+    Mat pinkPart; //detected pink - red part of the picture
+	
+    // HSV range use for the red detection
+	int hl = 0, hh = 20;
+	int hl2 = 170, hh2 = 180;
+	int sl = 100, sh = 255, vl = 20, vh = 255;
 
+	cvtColor(m, bgIsolation, CV_BGR2HSV); // Convert the image into HSV space
+	inRange(bgIsolation, Scalar(hl2, sl, vl), Scalar(hh2, sh, vh), pinkPart); 
+	inRange(bgIsolation, Scalar(hl, sl, vl), Scalar(hh, sh, vh), bgIsolation);
+
+	bitwise_or(bgIsolation, pinkPart, bgIsolation); // We add the two picture with an or mask to get a wider range of red colors
+    
+	morphologyEx(bgIsolation, bgIsolation, MORPH_CLOSE, Mat(), Point(-1, 1), 1, 0, 1); // Clean noise 
+	// \HSV
+
+	//CONTOURS
+	cv::Mat bw;
+	cv::Canny(bgIsolation, bw, 0, 50, 5);
+	// Find contours
+	std::vector<std::vector<cv::Point> > contours;
+	cv::findContours(bw.clone(), contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE); //Apply an edge detection on the picture
+	std::vector<cv::Point> approx;
+
+	for (int i = 0; i < contours.size(); i++)
+	{
+		// Approximate contour with accuracy proportional
+		// to the contour perimeter
+		cv::approxPolyDP(cv::Mat(contours[i]), approx, cv::arcLength(cv::Mat(contours[i]), true) * 0.02, true);
+		// Number of vertices of polygonal curve
+		int vtc = approx.size();
+
+		// Skip small or non-convex objects 
+		if (std::fabs(cv::contourArea(contours[i])) < 500 || !cv::isContourConvex(approx))
+			continue;
+
+		if (vtc == 3)
+		{
+            cv::Rect r = cv::boundingRect(contours[i]);
+			if (abs(r.width - r.height) < 50) { //look for a square bounding box
+                if (stopDuration.count() > 3000) { //check if we already have detected a triangle less than 3 seconds ago
+                    return 1;
+                }
+            }
+		}
+		else if (vtc ==4)
+		{		
+			// Get the cosines of all corners
+			std::vector<double> cos;
+			for (int j = 2; j < vtc + 1; j++)
+				cos.push_back(angle(approx[j % vtc], approx[j - 2], approx[j - 1]));
+
+			// Sort ascending the cosine values
+			std::sort(cos.begin(), cos.end());
+
+			// Get the lowest and the highest cosine
+			double mincos = cos.front();
+			double maxcos = cos.back();
+			
+			// Use the degrees obtained above and the number of vertices
+			// to determine the shape of the contour
+			if (mincos >= -0.1 && maxcos <= 0.3) {
+
+				cv::Rect r = cv::boundingRect(contours[i]);
+
+				if (abs(r.width - r.height) < 50) {
+					cv::Point pt(r.x + (r.width / 2), r.y + (r.height / 2)); //get center point of triangle
+					sqX = pt.x;
+					sqY = pt.y;
+					return 2;
+				}
+			}
+		}
+	}
+	//imshow("dst", bgIsolation);
+	return 0;
+	//\CONTOURS
+}
 int WhiteGroupsV2(Mat & m, Mat & edges, int& lX, int& lY, int& tries, int FOV){
 	
 	cvtColor(m, m, COLOR_BGR2GRAY); //Convert into grayscale
 
 	//Edge detection --------------------------------------------------------------------------------
 	adaptiveThreshold(m, edges, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY, 51, 7); 
-	//morphologyEx(edges, edges, MORPH_OPEN, Mat(), Point(-1, 1), 1, 0, 1);
 	morphologyEx(edges, edges, MORPH_GRADIENT, Mat(), Point(-1, 1), 1, 0, 1); //Close detected edges
 	bitwise_not(edges, edges); // Invert Edges
 		//imshow("Edges", edges); 
 	
 	//White detection --------------------------------------------------------------------------------
 	blur(m, m, Size(11, 11)); //Blur to reduce noises
-	threshold(m, m, 100, 255, 0); //Apply threshold to detect white pixels
+	threshold(m, m, 120, 255, 0); //Apply threshold to detect white pixels
 	morphologyEx(m, m, MORPH_CLOSE, Mat(), Point(-1, 1), 2, 0, 1); //reduce noises again
 		//imshow("thresh", m);
 
@@ -76,18 +294,6 @@ int WhiteGroupsV2(Mat & m, Mat & edges, int& lX, int& lY, int& tries, int FOV){
 	//Analyze Group of white pixel on the white + edge picture
 	Mat labels, stats, centroids;
 	connectedComponentsWithStats(m, labels, stats, centroids, 8);
-
-	/*for (int i = 0; i < stats.rows; i++)
-	{
-		if (stats.at<int>(Point(i, CC_STAT_AREA)) > 0) {
-			int x = stats.at<int>(Point(0, i));
-			int y = stats.at<int>(Point(1, i));
-			int w = stats.at<int>(Point(2, i));
-			int h = stats.at<int>(Point(3, i));
-			Rect rect(x, y, w, h);
-			rectangle(m, rect, 128);
-		}	 
-	}Draw rectangles on detected groups of white pixels*/
 
 	int mainI = 0, maxChange = m.cols/5, dis = 0, minDist = m.rows*100;
 	int x, y;
@@ -118,21 +324,6 @@ int WhiteGroupsV2(Mat & m, Mat & edges, int& lX, int& lY, int& tries, int FOV){
 	int theta = xToTheta(lX, m.cols, FOV);
 	//cout << theta << endl;
     return theta;
-}
-int RedTriangle(Mat & m)
-{
-	Mat ch1;
-
-	vector<Mat> channels(3);
-	split(m, channels);
-	// get the channels (dont forget they follow BGR order in OpenCV)
-	ch1 = channels[1];
-	channels[1] = Mat::zeros(m.rows, m.cols, CV_8UC1); // green channel is set to 0
-	channels[0] = Mat::zeros(m.rows, m.cols, CV_8UC1);// blue channel is set to 0
-
-	merge(channels, ch1);
-
-	//imshow("RED", ch1);
 }
 //-------------------LIDAR
 bool checkRPLIDARHealth(RPlidarDriver * drv)
@@ -167,12 +358,20 @@ void ctrlc(int)
 }
 //-------------------MAIN
 int main() {
-	//INIT CAM
-	int lX = 300;
-	int lY = 300;
+	//INIT CAM 
+	int lX = 300, lY = 300;
+    int sqX = 300, sqY = 300;
 	int tries = 0;
 	int FOV = 0;
     int theta = 0;
+
+    bool stopped = false;
+    bool lost = false;
+    bool Obstacle = false;
+    //bool veryLost = false;
+
+    int motorEnable = 1;
+    auto timerStop = std::chrono::high_resolution_clock::now();//timer since last stop signal
 	
 	Mat frame; // this will contain the image from the webcam
 	Mat edges; //contain calculated edges 
@@ -221,18 +420,8 @@ int main() {
 
     bool useArgcBaudrate = false;
 
-    printf("Ultra simple LIDAR data grabber for RPLIDAR.\n" " Version: "RPLIDAR_SDK_VERSION"\n");
+    printf("Ultra simple LIDAR data grabber for RPLIDAR.\n");
 
-    // read serial port from the command line...
-    /*if (argc>1) opt_com_path = argv[1]; // or set to a fixed value: e.g. "com3" 
-
-    // read baud rate from the command line if specified...
-    if (argc>2)
-    {
-        opt_com_baudrate = strtoul(argv[2], NULL, 10);
-        useArgcBaudrate = true;
-    }
-	*/
     opt_com_path = "/dev/ttyUSB0";
 
     // create the driver instance
@@ -335,7 +524,9 @@ int main() {
     int lastDis = disFollow;
     int cpterr = 0;
 
-    int thetaSent = theta;
+    //int thetaSent = theta;
+
+    thread t1; //thread used for the server when the car is lost
 
 	// MAIN LOOP OF THE PROGRAM
 	while (1) {
@@ -345,8 +536,25 @@ int main() {
 		camera >> frame;
 			//imshow("Webcam", frame);
 
-			//RedTriangle(frame);
-		theta = WhiteGroupsV2(frame, edges, lX, lY, tries, FOV);
+        auto t2 = std::chrono::high_resolution_clock::now();
+		auto int_stopDuration = std::chrono::duration_cast<std::chrono::milliseconds> (t2 - timerStop);
+		switch (RedTriangle(frame, sqX, sqY, int_stopDuration)) { // Check signal detection
+		case 0:
+			break;
+		case 1: //Triangle detected
+			timerStop = std::chrono::high_resolution_clock::now();
+			cout << "SSSSSTTTTOOOOOPPP" << endl;
+			stopped = !stopped;
+			break;
+		case 2: //Square detected
+			lX = sqX;
+			lY = sqY;
+			cout << "SQUAAARE" << endl;
+			break;
+		default:
+			break;
+		}
+		theta = WhiteGroupsV2(frame, edges, lX, lY, tries, FOV); // White pixel detection
 
 
 		//------------------------LIDAR PART
@@ -365,7 +573,6 @@ int main() {
 
             //Here we will take seven values around the angle that we are looking at
             list<int> values;
-            //vector<int> distances;
 
 			for(int i= -3; i<= 3; i ++){
                 int y = ((int)count + index + i)%(int)count;
@@ -373,12 +580,9 @@ int main() {
                 int distance = (int) nodes[y].dist_mm_q2/4.0f;
 
                 values.push_back(distance);
-                //distances.push_back((int)distance);
-
-                //cout << "angle " << i << " : " << (double) angle  << " distance: " << (int) distance << endl;
             }
 
-            cout << endl;
+            //cout << endl;
 
             // Here we get the minimal distance among the seven distances that we measured
 			int disMin = 0;
@@ -386,53 +590,101 @@ int main() {
             list<int>::iterator it = values.begin();
 
             for (int i = 0; i < values.size(); i++) {
-                if((*it <disMin && *it != 0) || (!hasChanged && *it != 0)){
+                //if((*it <disMin && *it != 0) || (!hasChanged && *it != 0)){
+                if((*it <disMin && *it >= 1000) || (!hasChanged && *it >= 1000)){
                     disMin = *it;
                     hasChanged =true;
                     index_min = i;
                 }
                 it++;
             }
-            if(disMin == 0){disMin = disFollow; index_min = values.size()/2;} // If we only have zeros on our values, we will send 2000 as a default value
-
+            //if(disMin == 0){disMin = disFollow; index_min = values.size()/2;} // If we only have zeros on our values, we will send 2000 as a default value
+            if(disMin == 0){
+                //disMin = lastDis;
+                disMin = 10000; 
+                index_min = values.size()/2;
+            }
             
-	    shift = index_min-3;
-            index = index + shift;
-            /*if(index > (int)count){index = index - (int)count;}
-            else if(index < 0){index = index + (int)count;}
-
-            cout << "deplacement = " << shift << "\n";
-            */
-            theta = index*((float)360/(float)count);
-            //index = (int)(theta/((float)360/(float)count)) ; //We calculate the angle at which the lidar will read distances according to the image analysis
-            cout << "distance min " << disMin << " at theta " << theta << " and index " << index <<"\n";
+	        //shift = index_min-3; //Used to update the angle of the lidar if the camera is not used
+            //index = index + shift; //Used to update the angle of the lidar if the camera is not used
+            
+            
+            //theta = index*((float)360/(float)count); // we calculate the angle from the lidar ( used instead of the camera in case there is a bug)
+            index = (int)(theta/((float)360/(float)count)) ; //We calculate the angle at which the lidar will read distances according to the image analysis
+            //cout << "distance min " << disMin << " at theta " << theta << " and index " << index <<"\n";
             cout << endl;
-            cout << endl;
+            //cout << endl;
 
-			if (abs(disMin - lastDis) >= 1000){ // avoid a huge difference beetween two consecutive values
+			if ((disMin - lastDis) >= 1000 || (lastDis - disMin)> 2000){ // avoid a huge difference beetween two consecutive values
                 disMin = lastDis;
                 cpterr++;
 
-                if (cpterr>4){lastDis = disFollow;}
-            }else{lastDis = disMin; cpterr = 0;}
+                if (cpterr>5){
+                    lost = true; 
+                    cpterr++; 
+
+                    if (cpterr>20){ // if we can't detect a valid target for too long the car get in very lost mode
+                        if(veryLost == false){
+                            veryLost = true;
+                            //Encode Image from the camera into base 64
+                            camera >> frame;
+                            imwrite("imageLost.jpg",frame);
+			    // Launch web server in thread
+                            t1 = thread(createServLost);
+                            cout << "VERY LOST"<<endl;
+                        }
+                        
+                        }
+                    }
+            }else{
+                lastDis = disMin; 
+                cpterr = 0; 
+                lost = false;
+
+                if(veryLost){
+                    cout<<"close serveur"<<endl;
+                    close(clientSocket);
+                    close(listening);
+                    listening = 0;
+                    clientSocket = 0;
+                    veryLost=false;
+                    t1.join();
+                }
+               
+            }
 
 
-            /* code to invert wheels direction when going backward
+            /* code to invert wheels direction when going backward NOT USED IN THE FINAL VERSION
             if(disMin < disFollow){thetaSent = -theta;}
             else{thetaSent = theta;}
             */
-            /*
-            // Obstacle avoidance
-            if (((int) nodes[0].dist_mm_q2/4.0f < 1200) && disMin > disFollow){
-                disMin = disFollow;
+            
+            
+            // Obstacle avoidance with lidar
+            //if (((int) nodes[0].dist_mm_q2/4.0f < 1200) && disMin > disFollow && ((int) nodes[0].dist_mm_q2/4.0f > 300)){
+                int av1= (int) nodes[0].dist_mm_q2/4.0f;
+                int av2 = (int) nodes[(int)count -1].dist_mm_q2/4.0f;
+                int av3 = (int) nodes[1].dist_mm_q2/4.0f;
+
+                int ar1= (int) nodes[(int)count/2].dist_mm_q2/4.0f;
+                int ar2 = (int) nodes[(int)count/2 - 1].dist_mm_q2/4.0f;
+                int ar3 = (int) nodes[(int)count/2 +1].dist_mm_q2/4.0f;
+            if ( ((av1 < 1200 && av1 > 300)||(av2 < 1200 && av2 > 300)||(av3 < 1200 && av3 > 300)) && disMin > disFollow){
+                //disMin = disFollow;
                 cout << " OBSTACLE" <<endl;
-            }else if(((int) nodes[(int)count/2].dist_mm_q2/4.0f < 500) && disMin < disFollow){
-                disMin = disFollow;
+                Obstacle = true;
+            }else if ( ((ar1 < 700 && ar1 > 100)||(ar2 < 700 && ar2 > 100)||(ar3 < 700 && ar3 > 100)) && disMin < disFollow){ 
+                //disMin = disFollow;
                 cout << " OBSTACLE" <<endl;
-            }
-            */
+                Obstacle = true;
+            }else {Obstacle = false;}
+            
+            
+
+            if(stopped || lost || Obstacle){motorEnable = 0;}
+            else{motorEnable = 1;}
             if (cpt > 5) { //we wait the 5th value (initial values are wrong) and we send the distance and the angle to the server at each loop
-                std::string tmp = std::to_string(disMin) + ":" + std::to_string(theta); //we send both values in a string with ":" as a separator
+                std::string tmp = std::to_string(disMin) + ":" + std::to_string(theta) + ":" + std::to_string(motorEnable); //we send both values in a string with ":" as a separator
                 msg = tmp.c_str();
 
                 send(sock , msg , strlen(msg) , 0 );
